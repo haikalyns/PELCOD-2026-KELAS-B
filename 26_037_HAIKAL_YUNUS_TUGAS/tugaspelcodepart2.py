143a2e5b14a80ce2efcd724fe0845bde6bd95370
haikalyns <haikalyunus0421@gmail.com>
2026-09-21 08:24:21 +0700

    26_037_HAIKAL_YUNUS_TUGAS

---
+++ 26_037_HAIKAL_YUNUS_TUGAS/tugaspelcodepart2.py
@@ -0,0 +1,20 @@
+nama = input("masukkan nama :")
+umur = int(input("masukkan umur :"))
+tinggi = int(input("masukkan tinggi :"))
+angka_fav = int(input("masukkan angka favorit kamu :"))
+
+# print("=========================")
+# pensil = int(input("masukkan jumlah pensil :"))
+# buku = int(input("masukkan jumlah buku :"))
+# hasil1 = pensil * 2000
+# hasil2 = buku * 5000
+
+# print("pensil :", hasil1)
+# print("buku :", hasil2)
+# print("total pembelian :", hasil1 + hasil2)
+
+print("==========================")
+if angka_fav % 2 == 0:
+    print("angka favorit anda adalah bilangan genap")
+else:
+    print("angka favorit anda adalah bilangan ganjil")
